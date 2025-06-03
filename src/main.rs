dae75427e8cd4d749207c3b312b8dba9c473013a
Sönke Hahn <soenkehahn@gmail.com>
2025-06-02 23:20:26 -0400

    First draft for niri script

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,46 @@
-#!/usr/bin/env rust-script
+mod niri;
+mod programs;
+mod sway;
+mod utils;
 
-use cradle::prelude::*;
 use std::path::Path;
-use std::thread::sleep;
-use std::time::Duration;
 use structopt::StructOpt;
-
-fn abort(message: &str) {
-    eprintln!("{}", message);
-    std::process::exit(1);
-}
-
-fn wait_for<F>(f: F)
-where
-    F: Fn() -> bool,
-{
-    let mut done = false;
-    while !done {
-        done = f();
-        sleep(Duration::from_secs_f32(0.05));
-    }
-}
-
-fn wait_for_change(sway_command: &str) {
-    let old = get_active_window();
-    swaymsg(sway_command);
-    eprintln!("waiting for {}...", sway_command);
-    wait_for(|| {
-        let new = get_active_window();
-        new != old
-    });
-    sleep(Duration::from_secs_f32(0.1));
-}
-
-fn get_active_window() -> String {
-    let StdoutUntrimmed(tree) = run_output!(%"swaymsg -t get_tree");
-    let StdoutTrimmed(id) = run_output!(
-        Stdin(tree),
-        "jq",
-        ".. | (.nodes? // empty)[] | select(.focused==true) | .id"
-    );
-    id
-}
-
-fn swaymsg(sway_command: &str) {
-    let StdoutTrimmed(result) = run_output!("swaymsg", sway_command);
-    let expected = "[\n  {\n    \"success\": true\n  }\n]";
-    if result != expected {
-        abort(&format!("{:?} != {:?}", result, expected));
-    }
-}
 
 #[derive(StructOpt)]
 struct Args {}
 
 fn main() {
+    let wm_type = WmType::get();
     let vcs_type = VcsType::get();
-    eprintln!("{:?}", vcs_type);
+    eprintln!("{:?}, {:?}", wm_type, vcs_type);
     let _args = Args::from_args();
-    swaymsg("layout tabbed");
-    wait_for_change("exec firefox --new-window github.com/notifications");
-    swaymsg("move left");
-    swaymsg("split toggle");
-    swaymsg("layout tabbed");
-    swaymsg("focus right");
-    // right shell for git
-    wait_for_change(&sway_terminal_command(None));
-    swaymsg("split toggle");
-    swaymsg("layout splith");
-    wait_for_change(&sway_terminal_command(match vcs_type {
-        VcsType::None => None,
-        VcsType::Git => Some("git-watch-tree"),
-        VcsType::Jujutsu => Some("/usr/bin/watch jj log --color always"),
-    }));
-    swaymsg("split toggle");
-    swaymsg("layout splitv");
-    wait_for_change(&sway_terminal_command(match vcs_type {
-        VcsType::None => None,
-        VcsType::Git => Some("/usr/bin/watch --color git -c color.ui=always status"),
-        VcsType::Jujutsu => {
-            Some("/usr/bin/watch bash -c '\"jj status --color always && jj diff --color always\"'")
-        }
-    }));
-    swaymsg("focus left");
-    swaymsg("focus left");
-    // middle tab
-    swaymsg("split toggle");
-    swaymsg("layout splith");
-    // shell top right
-    wait_for_change(&sway_terminal_command(None));
-    swaymsg("split toggle");
-    swaymsg("layout splitv");
-    swaymsg("focus left");
-    swaymsg("split toggle");
-    swaymsg("layout stacking");
-    swaymsg("resize set 60 ppt 0");
+    match wm_type {
+        WmType::Sway => sway::setup(vcs_type),
+        WmType::Niri => niri::setup(vcs_type),
+    }
     eprintln!("Done.");
 }
 
-const TERMINAL_COMMAND: &str = "alacritty";
-
-fn sway_terminal_command(command: Option<&str>) -> String {
-    let StdoutTrimmed(pwd) = run_output!("pwd");
-    let mut result = format!("exec {} --working-directory {}", TERMINAL_COMMAND, pwd);
-    if let Some(command) = command {
-        result.push_str(&format!(" -e {}", command));
-    }
-    result
+#[derive(Debug, Clone, Copy)]
+enum WmType {
+    Sway,
+    Niri,
 }
 
-#[derive(Debug)]
+impl WmType {
+    fn get() -> WmType {
+        match (
+            std::env::var_os("SWAYSOCK"),
+            std::env::var_os("NIRI_SOCKET"),
+        ) {
+            (Some(_), None) => WmType::Sway,
+            (None, Some(_)) => WmType::Niri,
+            _ => panic!("cannot detect window manager"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 enum VcsType {
     None,
     Git,
---
+++ src/programs.rs
@@ -0,0 +1,49 @@
+use crate::VcsType;
+use cradle::prelude::*;
+
+pub const TERMINAL_COMMAND: &str = "alacritty";
+
+pub fn terminal_command(command: Option<Vec<String>>) -> Vec<String> {
+    let StdoutTrimmed(pwd) = run_output!("pwd");
+    let mut result = vec![
+        TERMINAL_COMMAND.to_owned(),
+        "--working-directory".to_owned(),
+        pwd,
+    ];
+    if let Some(command) = command {
+        result.push("-e".to_owned());
+        result.extend(command);
+    }
+    result
+}
+
+pub fn tree(vcs_type: VcsType) -> Option<Vec<String>> {
+    match vcs_type {
+        VcsType::None => None,
+        VcsType::Git => Some(vec!["git-watch-tree".to_owned()]),
+        VcsType::Jujutsu => Some(
+            vec!["/usr/bin/watch", "jj", "log", "--color", "always"]
+                .into_iter()
+                .map(ToString::to_string)
+                .collect(),
+        ),
+    }
+}
+
+pub fn status(vcs_type: VcsType) -> Option<Vec<String>> {
+    match vcs_type {
+        VcsType::None => None,
+        VcsType::Git => Some(
+            "/usr/bin/watch --color git -c color.ui=always status"
+                .split_whitespace()
+                .map(ToString::to_string)
+                .collect(),
+        ),
+        VcsType::Jujutsu => Some(
+            "/usr/bin/watch bash -c '\"jj status --color always && jj diff --color always\"'"
+                .split_whitespace()
+                .map(ToString::to_string)
+                .collect(),
+        ),
+    }
+}
